5e795a5a10129102d8ac44845e7a0c345bf16d41
Geert van der Cruijsen <Geert van der Cruijsen>
2016-10-01 15:04:16 +0200

    initial commit of techdays demos for testcloud session on techdays

---
+++ WeatherDemo/MyWeather.UITest/AppInitializer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace MyWeather.UITest
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            var apkpath = "../../../MyWeather.Droid/bin/Release/MyWeather.Droid-Signed.apk";
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp
+                    .Android
+                    .EnableLocalScreenshots()
+                    //.Debug()
+                    .ApkFile(apkpath)
+                    .StartApp();
+            }
+
+            var appBundle = "../../../MyWeather.iOS/bin/iPhoneSimulator/Release/MyWeatheriOS.app";
+
+            return ConfigureApp
+                .iOS
+                .AppBundle(appBundle)
+                .EnableLocalScreenshots()
+                
+                .StartApp();
+        }
+    }
+}
+
---
+++ WeatherDemo/MyWeather.UITest/Tests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace MyWeather.UITest
+{
+    [TestFixture(Platform.Android)]
+    //[TestFixture(Platform.iOS)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]
+        public void AppLaunches()
+        {
+            app.Screenshot("First screen.");
+        }
+
+
+        [Test]
+        public void NewTest1()
+        {
+            app.Tap(x => x.Class("SwitchCompat"));
+            app.Tap(x => x.Text("amsterdam"));
+        }
+
+        [Test]
+        public void NewTest2()
+        {
+            app.Tap(x => x.Text("Seattle,WA"));
+            app.ClearText(x => x.Class("EntryEditText").Text("Seattle,WA"));
+            app.EnterText(x => x.Class("EntryEditText"), "amsterdam");
+            app.PressEnter();
+            app.Tap(x => x.Class("SwitchCompat"));
+            app.Tap(x => x.Text("Get Weather"));
+            
+            
+            app.Screenshot("ScreenshotEvent");
+            
+        }
+
+        [Test]
+        public void NewTest3()
+        {
+            app.Tap(x => x.Text("Seattle,WA"));
+            app.Tap(x => x.Text("Seattle,WA"));
+            app.ClearText(x => x.Class("EntryEditText").Text("Seattle,WA"));
+            app.EnterText(x => x.Class("EntryEditText"), "Amsterdam");
+            app.PressEnter();
+            app.Tap(x => x.Class("SwitchCompat"));
+            app.Tap(x => x.Text("Get Weather"));
+            app.WaitForElement(x => x.Marked("ConditionLabel"));
+            app.WaitForElement(x => x.Marked("TempLabel"));
+            app.Tap(x => x.Text("Amsterdam"));
+            app.ClearText(x => x.Class("EntryEditText").Text("Amsterdam"));
+            app.EnterText(x => x.Class("EntryEditText"), "blablabla");
+            app.Tap(x => x.Text("Get Weather"));
+        }
+
+
+    }
+}
+
